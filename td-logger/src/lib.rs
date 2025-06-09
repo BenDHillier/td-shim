8de18493a07c45f59e6774828cd9dd404b8607dd
Ben Hillier <bhillier@google.com>
2025-06-09 15:36:03 -0700

    Add serial logging initialization

--- td-logger/src/lib.rs
+++ td-logger/src/lib.rs
@@ -34,7 +34,46 @@ impl log::Log for LoggerBackend {
 /// Logger backend for the log crate
 static LOGGER_BACKEND: LoggerBackend = LoggerBackend;
 
+#[cfg(any(
+    all(not(feature = "tdx"), feature = "serial-port"),
+    all(
+        feature = "tdx",
+        not(feature = "tdg_dbg"),
+        not(feature = "no-tdvmcall")
+    ),
+    all(not(feature = "tdx"), feature = "serial-port")
+))]
+const SERIAL_IO_PORT: u16 = 0x3F8;
+const INTERRUPT_ENABLE_PORT: u16 = SERIAL_IO_PORT + 1;
+const FIFO_CONTROL_PORT: u16 = SERIAL_IO_PORT + 2;
+const LINE_CONTROL_PORT: u16 = SERIAL_IO_PORT + 3;
+const MODEM_CONTROL_PORT: u16 = SERIAL_IO_PORT + 4;
+
 pub fn init() -> Result<(), SetLoggerError> {
+    // Disable interrupts
+    tdx_tdcall::tdx::tdvmcall_io_write_8(INTERRUPT_ENABLE_PORT, 0 as u8);
+
+    // Enable the Divisor Latch Access Bit (DLAB) to configure baud rate.
+    // When DLAB is set, the Serial port and IE port function as the divisor
+    // latch registers
+    tdx_tdcall::tdx::tdvmcall_io_write_8(LINE_CONTROL_PORT, 0x80 as u8);
+    // Set baud rate to 115200 (DLL=1, DLM=0)
+    tdx_tdcall::tdx::tdvmcall_io_write_8(SERIAL_IO_PORT, 0x1 as u8);
+    tdx_tdcall::tdx::tdvmcall_io_write_8(INTERRUPT_ENABLE_PORT, 0 as u8);
+
+    // Disable DLAB and set word length to 8 bits
+    tdx_tdcall::tdx::tdvmcall_io_write_8(LINE_CONTROL_PORT, 0x3 as u8);
+
+    // Enable FIFO, clear TX/RX queues and
+    // set interrupt watermark at 14 bytes
+    tdx_tdcall::tdx::tdvmcall_io_write_8(FIFO_CONTROL_PORT, 0xc7 as u8);
+
+    // Set data terminal ready
+    tdx_tdcall::tdx::tdvmcall_io_write_8(MODEM_CONTROL_PORT, 0xb as u8);
+
+    // Enable interrupts
+    tdx_tdcall::tdx::tdvmcall_io_write_8(INTERRUPT_ENABLE_PORT, 1 as u8);
+
     log::set_logger(&LOGGER_BACKEND).map(|()| log::set_max_level(LevelFilter::Info))
 }
 
@@ -52,17 +91,6 @@ pub fn dbg_write_string(s: &str) {
         dbg_write_byte(c as u8);
     }
 }
-
-#[cfg(any(
-    all(not(feature = "tdx"), feature = "serial-port"),
-    all(
-        feature = "tdx",
-        not(feature = "tdg_dbg"),
-        not(feature = "no-tdvmcall")
-    ),
-    all(not(feature = "tdx"), feature = "serial-port")
-))]
-const SERIAL_IO_PORT: u16 = 0x3F8;
 
 // tdx, tdg_dbg
 #[cfg(all(feature = "tdx", feature = "tdg_dbg"))]
